713bb06141914160edc67b5cfabc0a9ecb3fcd79
SanCheetos <94627584+SanCheetos@users.noreply.github.com>
2024-10-18 18:38:21 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,14 @@ import pandas as pd
 
 st.image("titanic.jpg")
 df = pd.read_csv('titanic_train.csv', delimiter = ',')
+st.title("Подсчет среднего количества родственников, отдельно среди выживших и погибших")
+genre = st.radio(
+    "Выберите, у кого считаем",
+    ["Выжившие", "Погибшие"],
+)
+
+
+
+
+
 st.dataframe(df, use_container_width=True)
