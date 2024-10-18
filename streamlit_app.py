ff2e9a0fe29071d33274a328b76759d774ffea6c
SanCheetos <94627584+SanCheetos@users.noreply.github.com>
2024-10-18 19:04:31 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,14 @@ isSurvived = st.radio(
 
 if (isSurvived == "Выжившие"):
     needPeople = df[df['Survived'] == 1]
+    relatives = needPeople["SibSp"] + needPeople["Parch"]
+    meanRelatives = round(relatives.mean(), 2)
+    st.success("Среднее количество родственников: " + str(meanRelatives))
 else:
     needPeople = df[df['Survived'] == 0]
-relatives = needPeople["SibSp"] + needPeople["Parch"]
-meanRelatives = round(relatives.mean(), 2)
-st.write("Среднее количество родственников: " + str(meanRelatives))
+    relatives = needPeople["SibSp"] + needPeople["Parch"]
+    meanRelatives = round(relatives.mean(), 2)
+    st.error("Среднее количество родственников: " + str(meanRelatives))
+
+
 
