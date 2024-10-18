05dc06af3b393ca73b0e4c8cac9fa168c02bfd1f
SanCheetos <94627584+SanCheetos@users.noreply.github.com>
2024-10-18 19:12:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,21 @@ st.image("titanic.jpg")
 df = pd.read_csv('titanic_train.csv', delimiter = ',')
 st.title("Подсчет среднего количества родственников, отдельно среди выживших и погибших")
 isSurvived = st.radio(
-    "Выберите, у кого считаем",
-    ["Выжившие", "Погибшие"],
+    "Выберите пол",
+    ["Мужчина", "Женщина"],
 )
 
-if (isSurvived == "Выжившие"):
-    needPeople = df[df['Survived'] == 1]
-    relatives = needPeople["SibSp"] + needPeople["Parch"]
-    meanRelatives = round(relatives.mean(), 2)
-    st.success("Среднее количество родственников: " + str(meanRelatives))
+if (isSurvived == "Мужчина"):
+    sex = df[df['Sex'] == "male"]
+    
 else:
-    needPeople = df[df['Survived'] == 0]
-    relatives = needPeople["SibSp"] + needPeople["Parch"]
-    meanRelatives = round(relatives.mean(), 2)
-    st.error("Среднее количество родственников: " + str(meanRelatives))
+    sex = df[df['Sex'] == "female"]
+survivedPeople = sex[sex['Survived'] == 1]
+meanRelatives = round((survivedPeople["SibSp"] + survivedPeople["Parch"]).mean(), 2)
+st.write("Среднее количество родственников у выживших: " + str(meanRelatives))
+diedPeople = sex[sex['Survived'] == 0]
+meanRelatives = round((diedPeople["SibSp"] + diedPeople["Parch"]).mean(), 2)
+st.write("Среднее количество родственников у выживших: " + str(meanRelatives))
 
 
 
