b50aa4e4265398913c42f0cc24cb1a3eeae52600
SanCheetos <94627584+SanCheetos@users.noreply.github.com>
2024-11-01 19:01:18 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def test2():
             'Parch': [5, 2]
         }
     )
-    assert calcRelatives(dfTest, "Женщина") == [3,3.5]
+    assert calcRelatives(dfTest, "Женщина") == [6,7]
 
 test1()
 test2()
